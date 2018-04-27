729a2a6ace37e8978686ccaaa2f3e5c52bb82c26
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-27 02:32:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ function distanceFromHqInFeet (blocks) {
   return distanceFromHqInBlocks(blocks) * 264
 }
 
-function distanceTravelledInFeet (blocks) {
-  return distanceFromHqInBlocks(blocks) * 264
+function distanceTravelledInFeet (start, end) {
+  answer = (start - end) * 264
+  if (answer < 0) {
+    return answer * -1
+  }
+  else {
+    return answer
+  }
 }
